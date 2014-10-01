d385aaceebeee43d118f8c71b49d9647ad4a9089
pjones <pjones@198.105.244.228>
2014-10-01 19:17:55 -0400

    first draft of setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='udppd',
+      version='1.0',
+      description='Usenet Downloader post-processing daemon',
+      author='Phillip Jones',
+      author_email='udppd@dsmouse.com',
+      url="https://github.com/dsmouse/udppd",
+      my_modules="udppd"
+      
+     )
